f443784d810bee582a47685ce54f3533d9dcbb8f
Sara PG <39550635+pullz6@users.noreply.github.com>
2025-02-26 22:58:57 +0000

    Add files via upload

--- loading_data.py
+++ loading_data.py
@@ -1,1 +1,8 @@
 import pandas as pd 
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("anshtanwar/global-data-on-sustainable-energy")
+
+print("Path to dataset files:", path)
